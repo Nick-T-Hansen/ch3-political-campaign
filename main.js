59db731624f6a3b3fe379b0beee01f6a7b697aa1
Nick Hansen <nick.hansen13@gmail.com>
2018-11-26 11:26:59 -0600

    created possible data structure, needs testing

---
+++ main.js
@@ -0,0 +1,26 @@
+const volunteers = {
+    district: "5th",
+    platformStatements: {
+        taxes: "taxes are bad",
+        jobs: "jobs are good",
+        infrastructure: "infrastructure is cool",
+        healthCare: "healthy is neat",
+        crime: "crime makes me sad"
+    },
+    donateLink: "www.nickhansen.com",
+    eventCalendar: "www.google.com",
+    volunteerInfo: [
+        "Name",
+        "Address",
+        "Email",
+        "PhoneNumber",
+        "Availability",
+        "Activities",
+    ],
+    biography: "My name is Elizabeth",
+    imageGallery: [
+        "Headshot", "Picture of Family", "Picture of Constituents"
+    ],
+    missionStatement: "Make Tacos Great Again",
+    voteURL: "www.govotenow.com",
+}
