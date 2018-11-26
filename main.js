2138559fc43633b70edd1f06cf57c521f364cc33
Nick Hansen <nick.hansen13@gmail.com>
2018-11-26 11:44:57 -0600

    changed political name

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const volunteers = {
         availability: "Availability",
         activities: "Activities",
     },
-    biography: "My name is Elizabeth",
+    biography: "My name is Liz",
     imageGallery: {
         Headshot: "https://i.pinimg.com/originals/80/42/1f/80421fd6a102f2725a617de0878f6aa4.jpg",
         FamilyPicture: "https://www.google.com/search?q=picture+of+family&rlz=1C1XYJR_enUS812US812&tbm=isch&source=iu&ictx=1&fir=BVHfxzGMUpK4iM%253A%252COMFxKYwMf9m7PM%252C_&usg=AI4_-kQyE8--Uw3bTG5SKQeFIoyMG0ObyQ&sa=X&ved=2ahUKEwjCu_24zPLeAhWG11MKHdL3CscQ9QEwAHoECAUQBA#imgrc=BVHfxzGMUpK4iM:",
